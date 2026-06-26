99fc7985154431b831b9dda8e0d4c6ed666c5ae8
veeraboppudi <56449895+veeraboppudi@users.noreply.github.com>
2026-06-26 08:58:48 +0530

    Update pythonEx.py
    3rd

--- pythonEx.py
+++ pythonEx.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-pip3 install scikit-learn
+python -m pip install scikit-learn
 from sklearn.linear_model import LinearRegression
 
 # ---------------------------
